62fc1472834f1e42175a03533e3dd05db39547e3
Angelo Bovino <angelo.bovino@outlook.com>
2016-08-01 19:49:54 -0400

    adds .gitignore file and connects to JawsDB

--- UNSOLVED/app/config/connection.js
+++ UNSOLVED/app/config/connection.js
@@ -11,18 +11,21 @@ var source = {
         port: 3306,
         host: 'localhost',
         user: 'root',
-        password: "Njmitx123",
+        password: "olejuve123",
         database: "starwars"
     },
     jawsDB: {
-
-        // TODO: Add your JawsDB connection info here
+        port: 3306,
+        host: 'bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'ugetk0x0eoxvhd2a',
+        password: "a49pdhac8ov7r4fw",
+        database: 'ii0kw82fjm2wqddu'
     }
 }
 
 
 // TODO: connect your jawsDB connection witht he mysql connection.
-var connection = mysql.createConnection( //enter connection here);
+var connection = mysql.createConnection(source.jawsDB);
 
 
 
